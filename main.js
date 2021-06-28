1ab52f03732e78af91ec5173ac9f38ad14a1c357
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-06-28 13:00:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="Sourya";
+var last_name="Agrawal";
+var fullname= first_name.concat(last_name);
+console.log(fullname);
+var a= 10;
+var b= 5;
+console.log(a+b);
+var exponent= Math.pow(number,2);
+console.log(exponent);
+
